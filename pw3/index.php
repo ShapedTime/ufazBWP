458c1296a132797bc68aa26cbfb6df06b8e4d371
Teymur <teymur.hasanzada@wetravel.com>
2019-10-12 13:00:16 +0400

    pw3

--- pw3/index.php
+++ pw3/index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PW3</title>
+    <title>PW3: Student Timetable</title>
 </head>
 <body>
     <h1>Objective N1: PHP mysqli queries</h1>
@@ -19,11 +19,12 @@
     ?>
     <h2>Student timetable query</h2>
     <?
-        $query="SELECT * FROM catalog";
+        $query="SELECT classes.class_full_name, subjects.subject, professors.first_name, professors.last_name FROM courses WHERE courses.class_id = classes.class_id AND courses.subject_id = subjects.subject_id AND courses.professor_id = professors.professor_id";
         $procquery = mysqli_query($conn, $query) or die("Error in fetching query");
         echo "Number of rows for the query is: ".mysqli_num_rows($procquery);
-        
-
     ?>
+    <h1>Objective N2: HTML displayed results</h1>
+    <h2>HTML table to display the results</h2>
+
 </body>
 </html>
