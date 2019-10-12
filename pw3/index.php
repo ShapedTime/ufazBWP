130a97e2da659948dcf798b0085154f4e55d4245
Teymur <teymur.hasanzada@wetravel.com>
2019-10-12 13:30:16 +0400

    pw3

--- pw3/index.php
+++ pw3/index.php
@@ -19,11 +19,35 @@
     ?>
     <h2>Student timetable query</h2>
     <?
-        $query="SELECT classes.class_full_name, subjects.subject, professors.first_name, professors.last_name FROM courses, classes, subjects, professors WHERE courses.class_id = classes.class_id AND courses.subject_id = subjects.subject_id AND courses.professor_id = professors.professor_id";
+        $query="SELECT classes.class_full_name, subjects.subject, professors.first_name, professors.last_name, courses.course_date, courses.start_time, courses.end_time, courses.course_duration FROM courses, classes, subjects, professors WHERE courses.class_id = classes.class_id AND courses.subject_id = subjects.subject_id AND courses.professor_id = professors.professor_id";
         $procquery = mysqli_query($conn, $query) or die("Error in fetching query");
         echo "Number of rows for the query is: ".mysqli_num_rows($procquery);
     ?>
     <h1>Objective N2: HTML displayed results</h1>
+        <table>
+            <tr>
+                <th>Date of course</th>
+                <th>Starting Time</th>
+                <th>Ending Time</th>
+                <th>Course duration</th>
+                <th>Class name</th>
+                <th>Subject name</th>
+                <th>Professor name</th>
+            </tr>
+        <?
+            while($res=mysqli_fetch_assoc($procquery)){
+                echo "<tr>";
+                echo "<td>".$res["course_date"]."</td>";
+                echo "<td>".$res["start_time"]."</td>";
+                echo "<td>".$res["end_time"]."</td>";
+                echo "<td>".$res["course_duration"]."</td>";
+                echo "<td>".$res["class_full_name"]."</td>";
+                echo "<td>".$res["subject"]."</td>";
+                echo "<td>".$res["first_name"]." ".$res["last_name"]."</td>";
+                echo "</tr>";
+            }
+        ?>
+        </table>
     <h2>HTML table to display the results</h2>
 
 </body>
