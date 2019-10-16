dec3c99378fa415a2a3d16ec989c79346b6e320e
Teymur <teymur.hasanzada@wetravel.com>
2019-10-16 08:58:18 +0400

    pw4

---
+++ pw4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PW4</title>
+</head>
+<body>
+    <form>
+        <input type="radio" name="title" value="mr" checked>Mr.</input>
+        <input type="radio" name="title" value="mrs" checked>Mrs.</input>
+        
+    </form>
+</body>
+</html>
