28a3f83cd29013153a667ac2786e4a695d60e03b
Teymur <teymur.hasanzada@wetravel.com>
2019-11-21 10:49:26 +0400

    logo

--- hw_project/index.php
+++ hw_project/index.php
@@ -11,12 +11,15 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/568129426a.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="stylesheet.css">
     <title>Budget Table</title>
 </head>
 <body>
     <header>
-        <nav></nav>
+        <nav>
+            <i class="fas fa-money-check"></i>
+        </nav>
     </header>
 
     <footer></footer>
