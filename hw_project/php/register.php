78bbe65871b809ab490187e535b1aaadcc0f4bcf
Teymur <teymur.hasanzada@wetravel.com>
2019-11-22 20:59:02 +0400

    quickfix

--- hw_project/php/register.php
+++ hw_project/php/register.php
@@ -1,10 +1,10 @@
 <?
     include_once "mysqli_config.php";
-    if(isset($_POST["first-name"]) && isset($_POST["last-name"]) && isset($_POST["email"]) && isset($_POST["password"])){
-        if(!empty($_POST["first-name"]) && !empty($_POST["last-name"]) && !empty($_POST["email"]) && !empty($_POST["password"])){
+    if(isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["password"])){
+        if(!empty($_POST["firstname"]) && !empty($_POST["lastname"]) && !empty($_POST["email"]) && !empty($_POST["password"])){
             if ($stmt = $conn->prepare('INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)')) {
                 $pass = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                $stmt->bind_param('ssss', $_POST["first-name"], $_POST["last-name"], $_POST["email"], $pass);
+                $stmt->bind_param('ssss', $_POST["firstname"], $_POST["lastname"], $_POST["email"], $pass);
                 $stmt->execute();
                 // TODO: redirect to another page
             }else{
