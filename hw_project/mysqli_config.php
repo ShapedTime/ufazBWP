cac0784816ac78635488bec12b3c4d66a180a0e2
Teymur <teymur.hasanzada@wetravel.com>
2019-11-22 18:52:04 +0400

    change db name

--- hw_project/mysqli_config.php
+++ hw_project/mysqli_config.php
@@ -3,7 +3,7 @@
     $host="mysql-teymurufaz.alwaysdata.net";
     $user="169542";
     $pass="teymurufaz";
-    $db="teymurufaz_bwp";
+    $db="teymurufaz_hw_project";
     $conn=new mysqli($host, $user, $pass, $db);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
