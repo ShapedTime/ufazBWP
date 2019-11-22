51993b69056ff867dad64b6556043f267c34cc54
Teymur <teymur.hasanzada@wetravel.com>
2019-11-22 20:48:11 +0400

    quick fix

--- hw_project/php/ajax.php
+++ hw_project/php/ajax.php
@@ -19,7 +19,7 @@
     if(isset($_REQUEST["uniqueemail"])){
         if(!empty($_REQUEST["uniqueemail"])){
             if($stmt = $conn->prepare('SELECT `id` FROM `users` WHERE `email` = ?')){
-                $stmt->bindparam('s', $_REQUEST["uniqueemail"]);
+                $stmt->bind_param('s', $_REQUEST["uniqueemail"]);
                 $stmt->execute();
                 $stmt->store_result();
 
