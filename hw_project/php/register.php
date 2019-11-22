1c72ab248e1e8ef3975a60683f1b0af7e3fd6183
Teymur <teymur.hasanzada@wetravel.com>
2019-11-22 20:57:02 +0400

    quick fix

--- hw_project/php/register.php
+++ hw_project/php/register.php
@@ -2,7 +2,7 @@
     include_once "mysqli_config.php";
     if(isset($_POST["first-name"]) && isset($_POST["last-name"]) && isset($_POST["email"]) && isset($_POST["password"])){
         if(!empty($_POST["first-name"]) && !empty($_POST["last-name"]) && !empty($_POST["email"]) && !empty($_POST["password"])){
-            if ($stmt = $conn->prepare('INSERT INTO users (first_name, last_name, email, pass) VALUES (?, ?, ?, ?)')) {
+            if ($stmt = $conn->prepare('INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)')) {
                 $pass = password_hash($_POST["password"], PASSWORD_DEFAULT);
                 $stmt->bind_param('ssss', $_POST["first-name"], $_POST["last-name"], $_POST["email"], $pass);
                 $stmt->execute();
